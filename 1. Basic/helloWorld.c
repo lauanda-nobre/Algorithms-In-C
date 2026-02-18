407644c3730feec8d99be6d3688411f8168a47e4
lauanda1327 <lauanda.nobre@gmail.com>
2026-02-18 19:18:33 -0300

    first commit

---
+++ 1. Basic/helloWorld.c
@@ -0,0 +1,15 @@
+/*
+ Name: Hello World
+ Author: Lauanda Nobre
+ Date: 02/18/2026 or 18/02/2026
+ Purpose: The simple Hello World greeting
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("Press Enter to continue... ");
+    getchar();
+    return 0;
+}   
